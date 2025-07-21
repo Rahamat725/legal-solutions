b7c5fa59f37c6f42900c46f66fcfed0637ee2c3b
Sk Rahamatullah <skrahamatullah500@gmail.com>
2025-07-21 17:18:56 +0530

    setup ready

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#6cb2eb',
+        // 'primary-light':'#6cb2eb',
+      },
+    },
+  },
+};
